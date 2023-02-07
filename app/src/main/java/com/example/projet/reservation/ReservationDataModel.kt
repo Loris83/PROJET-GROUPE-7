5f4a14891e38a1dbb850a77b3670caea50f52022
Loris83 <loris.martel@isen.yncrea.fr>
2023-02-07 14:40:03 +0100

    commit

--- app/src/main/java/com/example/projet/reservation/ReservationDataModel.kt
+++ app/src/main/java/com/example/projet/reservation/ReservationDataModel.kt
@@ -1,9 +1,7 @@
 package com.example.projet.reservation
 
-data class ReservationDataModel(
-        val opening: String,
-        val ending: String,
-        val date: String,
-        val field_id: String,
-        val user_id: String
-)
+data class ReservationDataModel(val id: Int,
+                                val msg: String,
+                                val opening: String,
+                                val ending: String,
+                                val field: String)
